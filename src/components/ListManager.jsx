3a1cbe9473982b3d7c9332b5ed02a469c6c4b3d4
Joshy <josh@joshcruz.net>
2017-01-01 14:42:51 -0800

    fix css for dynamic page resizing

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -37,7 +37,7 @@ var ListManager = React.createClass({
     }
 
     return (
-      <div style={divStyle} className="col-sm-4">
+      <div style={divStyle} className="col-xs-12 col-sm-6 col-md-4">
         <div className="panel panel-primary">
           <div style={headingStyle} className="panel-heading">
             <h3>{this.props.title}</h3>
